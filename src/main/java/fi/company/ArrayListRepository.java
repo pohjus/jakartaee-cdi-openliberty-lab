048664fdef538ae821cab4835f0b35f670c4fa66
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-05-28 08:46:24 +0300

    commented thread testing away

--- src/main/java/fi/company/ArrayListRepository.java
+++ src/main/java/fi/company/ArrayListRepository.java
@@ -20,15 +20,15 @@ public class ArrayListRepository implements CrudRepository {
         return db;
     }
     public void save(String name) {
-        System.out.println(Thread.currentThread().getName());
-        for(int i=0; i<10; i++) {
-            System.out.println(i);
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        // System.out.println(Thread.currentThread().getName());
+        // for(int i=0; i<10; i++) {
+        //     System.out.println(i);
+        //     try {
+        //         Thread.sleep(1000);
+        //     } catch(InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
         db.add(name);
     }
 
